256d2202ff8a71057e975c3e86d56445e5899239
Wilx6 <wsuarez000@citymail.cuny.edu>
2023-12-01 00:43:35 -0500

    Added Animations to enemies

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerMovement : MonoBehaviour
 {
@@ -14,15 +15,15 @@ public class PlayerMovement : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-      rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     Move = Input.GetAxis("Horizontal");
+        Move = Input.GetAxis("Horizontal");
 
-     rb.velocity = new Vector2(Move * speed, rb.velocity.y);
+        rb.velocity = new Vector2(Move * speed, rb.velocity.y);
 
 
 
@@ -30,5 +31,17 @@ public class PlayerMovement : MonoBehaviour
      {
         rb.AddForce(new Vector2(rb.velocity.x, jump));
      }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Death1")
+        {
+            SceneManager.LoadScene(0);
+        }
+        if (other.tag == "NextLvl")
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 }
